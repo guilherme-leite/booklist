01e6910ffce7a88d58ddc653816e008bb8631ded
Guilherme Leite <guilhermelax@hotmail.com>
2022-03-01 09:54:44 -0300

    Fix: Alereracao desfeita

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -4,7 +4,7 @@ import { BookContext } from '../contexts/BookContext';
 const BookDetails = ({ book }) => {
   const { removeBook } = useContext(BookContext);
   return (
-    <li>
+    <li onClick={() => removeBook(book.id)}>
       <div className="title">{ book.title }</div>
       <div className="author">{ book.author }</div>
     </li>
