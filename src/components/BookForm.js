9150d243ec7d97c538ad9141a5d19a71d2087295
Guilherme Leite <guilhermelax@hotmail.com>
2022-03-01 16:21:07 -0300

    Feat: Implementando BookForm.js

---
+++ src/components/BookForm.js
@@ -0,0 +1,17 @@
+import React, { useContext, useState } from 'react';
+import { BookContext } from '../contexts/BookContext';
+
+const NewBookForm = () => {
+  const { addBook } = useContext(BookContext);
+  const [ title, setTitle ] = useState('');
+  const [ author, setAuthor ] = useState('');
+  return (
+    <form>
+      <input type="text" placeholder="Book Title" value={ title } onChange={ (e) => setTitle(e.target.value) } required />
+      <input type="text" placeholder="Book Author" value={ author } onChange={ (e) => setAuthor(e.target.value) } required />
+      <input type="submit" value="add book" />
+    </form>
+  );
+}
+ 
+export default NewBookForm;
