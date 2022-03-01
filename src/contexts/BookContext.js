854d04529e94186d151b5897dfb0ed2843307843
Guilherme Leite <guilhermelax@hotmail.com>
2022-03-01 09:53:08 -0300

    Fix: Adicionando spread no setBooks

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -5,14 +5,16 @@ export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
   const [ books, setBooks ] = useState([
-    {title: 'Name of the wind', author: 'Patrick Ruthfuss', id: 1}, 
-    {title: 'The final empire', author: 'Brandon Sanderson', id: 2}, 
+    {title: 'Name of the wind', author: 'Patrick Ruthfuss', id: 1},
+    {title: 'The final empire', author: 'Brandon Sanderson', id: 2},
+    {title: 'The final empire', author: 'Brandon Sanderson', id: 3},
+    {title: 'The final empire', author: 'Brandon Sanderson', id: 4},
   ]);
   const addBook = (title, author) => {
     setBooks([...books, {title: title, author: author, id: uuid() }]);
   };
   const removeBook = (id) => {
-    setBooks([books.filter(book => book.id !== id)]);
+    setBooks([...books.filter(book => book.id !== id)]);
   };
   return(
     <BookContext.Provider value={{  books, addBook, removeBook }}>
