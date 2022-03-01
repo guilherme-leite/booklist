74d65fb9ef3162cbc7edb39c180abea8f899022d
Guilherme Leite <guilhermelax@hotmail.com>
2022-03-01 18:16:42 -0300

    Feat: Criando pasta e reducer

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,22 +1,40 @@
-import React, { createContext, useState } from 'react';
-import { v1 as uuid } from 'uuid';
+// import React, { createContext, useState } from 'react';
+// // import { v1 as uuid } from 'uuid';
+
+// export const BookContext = createContext();
+
+// const BookContextProvider = (props) => {
+//   const [ books, setBooks ] = useState([
+//     {title: 'Name of the wind', author: 'Patrick Ruthfuss', id: 1},
+//     {title: 'The final empire', author: 'Brandon Sanderson', id: 2},
+//   ]);
+//   const addBook = (title, author) => {
+//     setBooks([...books, {title: title, author: author, id: uuid() }]);
+//   };
+//   const removeBook = (id) => {
+//     setBooks(books.filter(book => book.id !== id));
+//   };
+  
+//   return(
+//     <BookContext.Provider value={{  books, addBook, removeBook }}>
+//       { props.children }
+//     </BookContext.Provider>
+//   )
+// };
+
+// export default BookContextProvider;
+
+import React, { createContext, useReducer } from 'react';
+import { bookReducer } from '../reducers/bookReducer';
+// import { v1 as uuid } from 'uuid';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [ books, setBooks ] = useState([
-    {title: 'Name of the wind', author: 'Patrick Ruthfuss', id: 1},
-    {title: 'The final empire', author: 'Brandon Sanderson', id: 2},
-  ]);
-  const addBook = (title, author) => {
-    setBooks([...books, {title: title, author: author, id: uuid() }]);
-  };
-  const removeBook = (id) => {
-    setBooks(books.filter(book => book.id !== id));
-  };
+  const [ books, dispatch ] = useReducer(bookReducer, []);
   
   return(
-    <BookContext.Provider value={{  books, addBook, removeBook }}>
+    <BookContext.Provider value={{  books, dispatch }}>
       { props.children }
     </BookContext.Provider>
   )
