c7359af7fd9cce4e77443522ecdcadb3f3c9a313
Guilherme Leite <guilhermelax@hotmail.com>
2022-03-01 16:27:58 -0300

    Feat: Implementando handleSubmit que chama e seta a funcao addBook e cria nova li com info

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -5,8 +5,14 @@ const BookForm = () => {
   const { addBook } = useContext(BookContext);
   const [ title, setTitle ] = useState('');
   const [ author, setAuthor ] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addBook(title, author);
+    setTitle('');
+    setAuthor('');
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" placeholder="Book Title" value={ title } onChange={ (e) => setTitle(e.target.value) } required />
       <input type="text" placeholder="Book Author" value={ author } onChange={ (e) => setAuthor(e.target.value) } required />
       <input type="submit" value="add book" />
