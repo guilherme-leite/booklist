688d7b64c47fee7739acca15318352d847f479e6
Guilherme Leite <guilhermelax@hotmail.com>
2022-03-01 18:21:13 -0300

    Feat: Refatorando BookForm

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,13 +1,39 @@
+// import React, { useContext, useState } from 'react';
+// import { BookContext } from '../contexts/BookContext';
+
+// const BookForm = () => {
+//   const { addBook } = useContext(BookContext);
+//   const [ title, setTitle ] = useState('');
+//   const [ author, setAuthor ] = useState('');
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     addBook(title, author);
+//     setTitle('');
+//     setAuthor('');
+//   }
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <input type="text" placeholder="Book Title" value={ title } onChange={ (e) => setTitle(e.target.value) } required />
+//       <input type="text" placeholder="Book Author" value={ author } onChange={ (e) => setAuthor(e.target.value) } required />
+//       <input type="submit" value="add book" />
+//     </form>
+//   );
+// }
+ 
+// export default BookForm;
+
 import React, { useContext, useState } from 'react';
 import { BookContext } from '../contexts/BookContext';
 
 const BookForm = () => {
-  const { addBook } = useContext(BookContext);
+  const { dispatch } = useContext(BookContext);
   const [ title, setTitle ] = useState('');
   const [ author, setAuthor ] = useState('');
   const handleSubmit = (e) => {
     e.preventDefault();
-    addBook(title, author);
+    dispatch({type: 'ADD_BOOK', book: {
+      title: title, author: author,
+    }});
     setTitle('');
     setAuthor('');
   }
