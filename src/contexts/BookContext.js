86a3f5430ed0019f64757ccb0f0f7ed3409945dd
Guilherme Leite <guilhermelax@hotmail.com>
2022-03-01 18:36:26 -0300

    Feat: Implementantando useEffect e logica de salvar no localStorage a partir do BookContext

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -26,15 +26,22 @@
 
 // Refatorando para utilizar o useReducer.
 
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { bookReducer } from '../reducers/bookReducer';
 // import { v1 as uuid } from 'uuid';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [ books, dispatch ] = useReducer(bookReducer, []);
-  
+  const [ books, dispatch ] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem('books');
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('books', JSON.stringify(books));
+  }, [books]);
+
   return(
     <BookContext.Provider value={{  books, dispatch }}>
       { props.children }
